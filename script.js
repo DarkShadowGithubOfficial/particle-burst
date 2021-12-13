e49ddb374cd3e9f6a60d1dd4a2df7a2c448444ed
Dark Shadow <96031279+DarkShadowGithubOfficial@users.noreply.github.com>
2021-12-13 12:09:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-var canvas = document.querySelector('#canvas');
-var btn = document.querySelector('#btn');
-var ctx = canvas.getContext('2d');
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Particle {
-  constructor(centerX, centerY, color) {
-    this.color = color;
+  constructor(centerX, centerY) {
     this.x = centerX + Math.random() * 10 - 5;
-    this.y = y + centerY + Math.random() * 10 - 5;
+    this.y = centerY + Math.random() * 10 - 5;
     this.dx = Math.random() * 4 - 2;
-    this.size = Math.random() * 5 + 5;
+    this.size = 7.5;
     this.dy = Math.random() * 4 - 2;
   } draw() {
     ctx.beginPath();
-    ctx.fillStyle = this.color;
+    ctx.fillStyle = 'hsl(' + currentHue + ', 100%, 50%)';
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill
+    ctx.fill();
   } update() {
     this.x += this.dx;
     this.y += this.dy;
   }
 }
 let currentHue = 0;
-var particles = [];
-function init(index) {
-  for (let i = 0; i < index; i++) {
+let particles = [];
+function init() {
+  for (let i = 0; i < 2; i++) {
     let centX = Math.random() * canvas.width;
     let centY = Math.random() * canvas.height;
-    particles.push(new Particle(centX, centY, 'hsl(' + currentHue + ', 100%, 50%)'));
+    particles.push(new Particle(centX, centY));
   }
 }
-btn.addEventListener('click', () => {
-  init(6);
-})
 function animate() {
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  init();
   for (let i = 0; i < particles.length; i++) {
     particles[i].draw();
     particles[i].update();
